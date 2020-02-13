cbc58948c8a492b3681d81d692022efad29aa55d
zsadov1 <zsadov1@gmail.com>
2020-02-13 16:05:10 -0500

    Connection.js Setup

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,23 @@
 const mysql = require("mysql");
 
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "",
+    database: "burger_db"
+});
 
+// Make Connection
 
+connection.connect(function(err) {
+    if(err) {
+        console.error("error connecting:" + err.stack);
+        return;
+    }
+    console.log("connected as id" + connection.threadId);
+});
 
-module.exports = mysql; 
+// Export connection for our ORM to use
+
+module.exports = connection; 
