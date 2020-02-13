d5e34f725b02fbda80eb992d1e547dd83439d3e8
zsadov1 <zsadov1@gmail.com>
2020-02-13 16:59:42 -0500

    Burger.js and CSS complete

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,25 @@
 const orm = require("../config/orm");
 
 
+const burger = {
+    SelectAll: function(callback) {
+        orm.selectAll("burgers" , function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(inputValue , callback) {
+        orm.insertOne("burgers" , "burgers_name", inputValue , function(res) {
+            callback(res);
+        });
+    },
+
+    updateOne: function(id, callback) {
+        orm.updateOne("burgers", "devoured", 1, "id" , id, function(res) {
+            callback(res);
+        });
+    }
+
+};
+
 module.exports = burger; 
