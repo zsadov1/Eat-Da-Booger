5cecf19dcc3e7e3e09f30754bd0c1a7b094c337c
zsadov1 <zsadov1@gmail.com>
2020-02-13 17:10:05 -0500

    Updated Server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express"); 
+
+let PORT = process.env.PORT || 8080;
+
+const app = express();
+
+appuse(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars" , exphbs({defaultLayout: "main"}));
+app.set("view engine" , "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
