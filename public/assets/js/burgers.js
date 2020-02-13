95d85417e67d1ebfa9029153b23343cb4adeafc9
zsadov1 <zsadov1@gmail.com>
2020-02-13 17:04:45 -0500

    Update CSS, Burger.js and Main handlebars

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newDevoured");
+  
+      var newDevourState = {
+        devoured: newDevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
